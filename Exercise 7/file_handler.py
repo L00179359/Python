2cf59fd176e91ae89e4bb5b383c9ccc9a347a303
rahulkharade <rahul.ibmaix@gmail.com>
2023-12-16 23:44:23 +0000

    new commit for exercise 7

---
+++ Exercise 7/file_handler.py
@@ -0,0 +1,28 @@
+'''
+Basic file handing
+By: Rahul Kharade
+16 DEC 2023
+'''
+
+#Declare file name
+my_filename = "logfile.txt"
+
+#use try function
+try:
+    with open(my_filename, "a") as file_handle:
+        print(f"Writing a test line to {my_filename}")
+        file_handle.write("Test line")
+except IOError as err:
+    print(f"IOError was {err}")
+except EOFError as err:
+    print(f"End of file error was {err}")
+except OSError:
+    print("OS Error")
+except:
+    print("General Error")
+else:
+    print("File created")
+finally:
+    print("Finishing up!")
+    # close not needed because with statement
+    # file_handle.close()
